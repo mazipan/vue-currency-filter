31b735334a66d82f0c9950a9bd15569f9042ab58
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-08-02 10:03:57 +0700

    Change demo

--- main.js
+++ main.js
@@ -9,9 +9,76 @@ new Vue({
   el: '#app',
   template: `    
     <div class="VueCurrencyFilter">
-      <h1>VueCurrencyFilter Demo</h1>
+      <h1>Lightweight and Customizable Vue Currency Filter Plugins By Irfan Maulana</h1>
+
+      <h2>Example : </h2>
       <div>Before filter : 20000</div>
       <div>After filter : {{ 20000 | currency}}</div>
+
+      Using setup :
+      <pre>
+      	Vue.use(VueCurrencyFilter, {symbol: '$'})
+      </pre>
+
+      <div class="grid__row content centered">
+	      <h2>Contribute</h2>
+
+	      <p>
+	        Feel free to fork <i class="fa fa-code-fork"></i> on <a href="https://github.com/mazipan/vue-currency-filter"
+	       target="_blank">GitHub <i class="fa fa-github"></i></a> if you have any features <i class="fa fa-cart-plus"></i> or bugs <i class="fa fa-bug"></i>!    
+	      </p>      
+
+	    </div>
+
+	    <div class="grid__row content centered">
+	      <h2>Contact Developer</h2>
+
+	      <p>    
+
+	        <a href="https://github.com/mazipan">
+	          <img src="https://img.shields.io/badge/mazipan-Github-lightgrey.svg?maxAge=3600" 
+	            alt="Github">
+	        </a>
+
+	        <a href="mailto:mazipanneh@gmail.com">
+	          <img src="https://img.shields.io/badge/mazipanneh-Email-yellow.svg?maxAge=3600" 
+	            alt="Email">
+	        </a>
+
+	        <a href="https://mazipanneh.com/blog/">
+	          <img src="https://img.shields.io/badge/mazipanneh-Blog-brightgreen.svg?maxAge=3600"   alt="Blog">
+	        </a>
+
+	        <a href="https://facebook.com/mazipanneh">
+	          <img src="https://img.shields.io/badge/mazipanneh-Facebook-blue.svg?maxAge=3600" 
+	            alt="Facebook">
+	        </a>
+
+	        <br/>
+
+	        <a href="https://twitter.com/Maz_Ipan">
+	          <img src="https://img.shields.io/badge/Maz_Ipan-Twitter-55acee.svg?maxAge=3600" 
+	            alt="Twitter">
+	        </a>  
+
+	        <a href="https://id.linkedin.com/in/irfanmaulanamazipan">
+	          <img src="https://img.shields.io/badge/irfanmaulanamazipan-Linkedin-0077b5.svg?maxAge=3600" 
+	            alt="Linkedin">
+	        </a> 
+
+	        <a href="https://www.slideshare.net/IrfanMaulana21">
+	          <img src="https://img.shields.io/badge/IrfanMaulana21-Slideshare-0077b5.svg?maxAge=3600" 
+	            alt="Slideshare">
+	        </a>  
+	      </p>      
+
+	    </div>
+
+	    <div class="grid__row content centered">
+	      <p>
+	        Copyright © 2017 <a href="https://mazipanneh.com/blog/">Irfan Maulana</a>, All Rights Reserved.   
+	      </p>      
+	    </div>
     </div>
   `,
 })
