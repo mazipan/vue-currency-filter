aa87a95042d9eccc28b7c17bd180102bc515f166
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-12-24 02:25:54 +0700

    add unit test case

--- test/VueCurrencyFilter.spec.js
+++ test/VueCurrencyFilter.spec.js
@@ -60,6 +60,99 @@ describe('test VueCurrencyFilter', () => {
     wrapper.setData({ configSeparator: 5 })
 
     let result = wrapper.find('.result__filter')
+    expect(result.text()).toEqual('$ 55000')
+  })
+  it('Test symbolPosition:front symbolSpacing:true', () => {    
+    let localVue = createLocalVue()
+    localVue.use(VueCurrencyFilter)
+
+    let wrapper = shallow(App, {
+      localVue
+    })
+
+    wrapper.setData({ textInput: 5000 })
+    wrapper.setData({ configSymbolPosition: 'front' })
+    wrapper.setData({ configSymbolSpacing: true })
+
+    let result = wrapper.find('.result__filter')
+    expect(result.text()).toEqual('$ 5.000')
+  })
+  it('Test symbolPosition:front symbolSpacing:false', () => {    
+    let localVue = createLocalVue()
+    localVue.use(VueCurrencyFilter)
+
+    let wrapper = shallow(App, {
+      localVue
+    })
+
+    wrapper.setData({ textInput: 5000 })
+    wrapper.setData({ configSymbolPosition: 'front' })
+    wrapper.setData({ configSymbolSpacing: false })
+
+    let result = wrapper.find('.result__filter')
+    expect(result.text()).toEqual('$5.000')
+  })
+  it('Test symbolPosition:end symbolSpacing:false', () => {    
+    let localVue = createLocalVue()
+    localVue.use(VueCurrencyFilter)
+
+    let wrapper = shallow(App, {
+      localVue
+    })
+
+    wrapper.setData({ textInput: 5000 })
+    wrapper.setData({ configSymbolPosition: 'end' })
+    wrapper.setData({ configSymbolSpacing: false })
+
+    let result = wrapper.find('.result__filter')
+    expect(result.text()).toEqual('5.000$')
+  })
+  it('Test symbolPosition:end symbolSpacing:true', () => {    
+    let localVue = createLocalVue()
+    localVue.use(VueCurrencyFilter)
+
+    let wrapper = shallow(App, {
+      localVue
+    })
+
+    wrapper.setData({ textInput: 5000 })
+    wrapper.setData({ configSymbolPosition: 'end' })
+    wrapper.setData({ configSymbolSpacing: true })
+
+    let result = wrapper.find('.result__filter')
+    expect(result.text()).toEqual('5.000 $')
+  })
+  it('Test value isNan', () => {    
+    let localVue = createLocalVue()
+    localVue.use(VueCurrencyFilter)
+
+    let wrapper = shallow(App, {
+      localVue
+    })
+
+    wrapper.setData({ textInput: 'NotANumber' })
+
+    let result = wrapper.find('.result__filter')
+    expect(result.text()).toEqual('$ 0')
+  })
+  // should be in last test case
+  it('Test set all config to undefined', () => {    
+    let localVue = createLocalVue()
+    localVue.use(VueCurrencyFilter)
+
+    let wrapper = shallow(App, {
+      localVue
+    })
+
+    wrapper.setData({ textInput: 5000 })
+    wrapper.setData({ configSymbol: undefined })
+    wrapper.setData({ configSeparator: undefined})
+    wrapper.setData({ configFractionCount: undefined })
+    wrapper.setData({ configFractionSeparator: undefined })
+    wrapper.setData({ configSymbolPosition: undefined })
+    wrapper.setData({ configSymbolSpacing: undefined })
+
+    let result = wrapper.find('.result__filter')
     expect(result.text()).toEqual('$ 5.000')
   })
 });
