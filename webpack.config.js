e85e1d5e95101ee73381ff22b97dc54fbf5ef1de
Gijs Rogé <rogegijs@gmail.com>
2019-04-22 10:19:59 +0200

    Add target to node as we don't need the Window object. (#54)
    This fixes issues with SSR frameworks that don't have the Window object
    available during the build phase.

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   entry: {
     VueCurrencyFilter: './src/VueCurrencyFilter.js',
   },
+  target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist',
