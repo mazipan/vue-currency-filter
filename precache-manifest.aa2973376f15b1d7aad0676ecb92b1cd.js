2fcbdb0cff6cc10bd0df502aaa6d183d9f3967cb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-27 07:51:31 +0000

    Deploy mazipan/vue-currency-filter to github.com/mazipan/vue-currency-filter.git:gh-pages

---
+++ precache-manifest.aa2973376f15b1d7aad0676ecb92b1cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839f880b9d69e3ff8b3e",
+    "url": "/css/app.83db837b.css"
+  },
+  {
+    "revision": "62c064aa3c84d19ef2dd",
+    "url": "/css/chunk-vendors.a189a293.css"
+  },
+  {
+    "revision": "78d2ab9641ce1a0df5d66cb49e069221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b8925b3d063003d4300",
+    "url": "/js/about.30611e3a.js"
+  },
+  {
+    "revision": "839f880b9d69e3ff8b3e",
+    "url": "/js/app.16260322.js"
+  },
+  {
+    "revision": "62c064aa3c84d19ef2dd",
+    "url": "/js/chunk-vendors.b53133d8.js"
+  },
+  {
+    "revision": "d1eb1d1176f735a72f57cd087e538372",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
