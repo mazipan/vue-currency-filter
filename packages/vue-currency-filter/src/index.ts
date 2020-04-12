9f2f9c5d8cd95c3ae8b7511197c13aa6a8ac508b
sysaib <sysa.i@wezom.com.ua>
2020-04-12 23:50:38 +0300

    multiple instance from array fix for nuxt 2.12.1

--- packages/vue-currency-filter/src/index.ts
+++ packages/vue-currency-filter/src/index.ts
@@ -12,80 +12,88 @@ const defaultConfig = {
 }
 
 const VueCurrencyFilter = {
-  install (Vue, options) {
-    if (__isNull(options)) options = {}
-    const globalConfigs = __defaults(options, defaultConfig)
-    let { name, ...configs } = globalConfigs
+  install (Vue, pluginOptions) {
+    const createFilter = (options) => {
+      if (__isNull(options)) options = {}
+      const globalConfigs = __defaults(options, defaultConfig)
+      let { name, ...configs } = globalConfigs
 
-    const filterCurrency = function (value,
-      _symbol,
-      _thousandsSeparator,
-      _fractionCount,
-      _fractionSeparator,
-      _symbolPosition,
-      _symbolSpacing) {
+      const filterCurrency = function (value,
+                                       _symbol,
+                                       _thousandsSeparator,
+                                       _fractionCount,
+                                       _fractionSeparator,
+                                       _symbolPosition,
+                                       _symbolSpacing) {
 
-      let runtimeConfig = __defaults({
-        symbol: _symbol,
-        thousandsSeparator: _thousandsSeparator,
-        fractionCount: _fractionCount,
-        fractionSeparator: _fractionSeparator,
-        symbolPosition: _symbolPosition,
-        symbolSpacing: _symbolSpacing
-      }, configs)
+        let runtimeConfig = __defaults({
+          symbol: _symbol,
+          thousandsSeparator: _thousandsSeparator,
+          fractionCount: _fractionCount,
+          fractionSeparator: _fractionSeparator,
+          symbolPosition: _symbolPosition,
+          symbolSpacing: _symbolSpacing
+        }, configs)
 
-      if (typeof _symbol === 'object') {
-        runtimeConfig = __defaults(_symbol, configs)
-      }
+        if (typeof _symbol === 'object') {
+          runtimeConfig = __defaults(_symbol, configs)
+        }
 
-      let result = 0.0
-      const isNegative = String(value).charAt(0) === '-'
+        let result = 0.0
+        const isNegative = String(value).charAt(0) === '-'
 
-      if (isNegative) {
-        value = String(value).slice(1)
-      }
+        if (isNegative) {
+          value = String(value).slice(1)
+        }
 
-      const amount = parseFloat(value)
-      if (!isNaN(amount)) {
-        result = amount
-      }
+        const amount = parseFloat(value)
+        if (!isNaN(amount)) {
+          result = amount
+        }
 
-      let formatConfig = '%s%v'
-      if (runtimeConfig.symbolPosition === 'front') {
-        formatConfig = runtimeConfig.symbolSpacing ? '%s %v' : '%s%v'
-      } else {
-        formatConfig = runtimeConfig.symbolSpacing ? '%v %s' : '%v%s'
-      }
+        let formatConfig = '%s%v'
+        if (runtimeConfig.symbolPosition === 'front') {
+          formatConfig = runtimeConfig.symbolSpacing ? '%s %v' : '%s%v'
+        } else {
+          formatConfig = runtimeConfig.symbolSpacing ? '%v %s' : '%v%s'
+        }
 
-      if (runtimeConfig.fractionCount > 0) {
-        value = toFixed(value, runtimeConfig.fractionCount)
-      }
+        if (runtimeConfig.fractionCount > 0) {
+          value = toFixed(value, runtimeConfig.fractionCount)
+        }
 
-      // @ts-ignore
-      result = formatMoney(value, {
-        format: formatConfig,
-        symbol: runtimeConfig.symbol,
-        precision: runtimeConfig.fractionCount,
-        thousand: runtimeConfig.thousandsSeparator,
-        decimal: runtimeConfig.fractionSeparator
-      })
-
-      if (isNegative) {
         // @ts-ignore
-        result = '-' + result
+        result = formatMoney(value, {
+          format: formatConfig,
+          symbol: runtimeConfig.symbol,
+          precision: runtimeConfig.fractionCount,
+          thousand: runtimeConfig.thousandsSeparator,
+          decimal: runtimeConfig.fractionSeparator
+        })
+
+        if (isNegative) {
+          // @ts-ignore
+          result = '-' + result
+        }
+
+        return result
       }
 
-      return result
+      Vue.filter(name, filterCurrency)
+      Vue.prototype['$' + name] = {
+        setConfig: (options) => {
+          configs = __defaults(options, defaultConfig)
+        },
+        getConfig: () => {
+          return configs
+        }
+      }
     }
 
-    Vue.filter(name, filterCurrency)
-    Vue.prototype.$CurrencyFilter = {
-      setConfig: (options) => {
-        configs = __defaults(options, defaultConfig)
-      },
-      getConfig: () => {
-        return configs
-      }
+    if (Array.isArray(pluginOptions)) {
+      pluginOptions.forEach(options => createFilter(options))
+    } else {
+      createFilter(pluginOptions)
     }
   }
 }
